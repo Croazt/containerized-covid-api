bc0aef58acd029dc6c4f1c8083ef28e193dc866d
Croazt <fachry.croazt@gmail.com>
2022-01-07 19:21:28 +0700

    [fachry] feat(covid_data) : creating test for extracting covid_data to general information of covid cases

--- tests/covid_data_test.py
+++ tests/covid_data_test.py
@@ -1,5 +1,6 @@
 import unittest
 import json
+import pandas as pd
 from src.driver.covid_data import CovidDataDriver
 class TestCovidDataRequests(unittest.TestCase):
 
@@ -12,3 +13,11 @@ class TestCovidDataRequests(unittest.TestCase):
         data = CovidDataDriver()
         response = data.get_data()
         assert len(json.loads(response['data'])) > 0
+    
+    total_df_template = pd.DataFrame(columns=['total_positive', 'total_recovered', 'total_deaths', 'total_active', 'new_positive', 'new_recovered', 'new_deaths', 'new_active'])
+
+    def test_covid_data_extract_total_returns_dataframe(self) :
+        driver = CovidDataDriver()
+        data = data.get_data_total()
+        pd.testing.assert_frame_equal(total_df_template, data)
+
