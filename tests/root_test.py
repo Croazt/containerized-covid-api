a29cf14dc8578d513cbf80b578bddc9799484240
Croazt <fachry.croazt@gmail.com>
2022-01-08 23:36:53 +0700

    [fachry] feat(root_test) : creating test for root endpoint where checking returned value is general data result

--- tests/root_test.py
+++ tests/root_test.py
@@ -9,3 +9,10 @@ class TestRoot(unittest.TestCase):
         with app.test_client() as client:
             response = client.get('/')
             assert response._status_code == 200
+
+    def test_root_endpoint_returns_general_data_result_in_response(self):
+        with app.test_client() as client:
+            response = client.get('/')
+            data = json.loads(response.get_data())
+            assert len(data['data'] ) > 0
+            assert ('total_positive' in data['data'][0]) and ('total_recovered' in data['data'][0]) and ('total_deaths' in data['data'][0]) and ('total_active' in data['data'][0])
