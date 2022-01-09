c1801c69ce126dd616c66562a2bd26e9b51605d7
Croazt <fachry.croazt@gmail.com>
2022-01-09 21:37:34 +0700

    [fachry] feat(periodically_data_interface) : creating get_daily_data abstract function

--- src/interface/repository/periodically_data_repository.py
+++ src/interface/repository/periodically_data_repository.py
@@ -8,3 +8,7 @@ class PeriodicallyDataRepository(metaclass=ABCMeta):
     @abstractmethod
     def get_monthly_data(self, since, upto) -> dict :
         raise NotImplementedError
+        
+    @abstractmethod
+    def get_daily_data(self, since, upto) -> dict :
+        raise NotImplementedError
