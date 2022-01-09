fa684a36096965b8af8dd926b380960b89b3a4b8
Croazt <fachry.croazt@gmail.com>
2022-01-09 17:15:22 +0700

    [fachry] feat(periodically_data) : creating some testing for periodically_data_repository

--- tests/periodically_data_test.py
+++ tests/periodically_data_test.py
@@ -2,6 +2,7 @@ import unittest
 import json
 import pandas as pd
 import re
+import datetime as dt
 
 from src.driver.covid_data import CovidDataDriver
 from src.repository.periodically_data_repository import PeriodicallyDataRepository, replace_fillz
@@ -89,6 +90,31 @@ class MonthlyData(unittest.TestCase):
     def test_periodically_data_repository_given_string_returning_modified_string(self):
         word = replace_fillz('2020.1')
         pattern = re.compile("^[0-9][0-9][0-9][0-9].[0-9][0-9]")
-        
+
         assert pattern.match(word)
         assert word == "2020-01"
+
+    def test_periodically_data_repository_given_since_value_returns_Monthly_object_between_the_range_of_since_and_date_now(self):
+        repository = PeriodicallyDataRepository(covid_driver=CovidDataDriver())
+        data = repository.get_monthly_data(since="2021.1")
+        date = dt.datetime.now()
+        monthup = str(date.year) + '-' + str(date.month).zfill(2)
+        monthsin =  replace_fillz('2021.1')
+
+        self.assertIsInstance(data, Monthly)
+        assert data.values[0]['month'] == monthsin
+        assert data.values[-1]['month'] == monthup
+
+    def test_periodically_data_repository_given_since_and_value_returns_Monthly_object_between_the_range_of_since_and_upto(self):
+        repository = PeriodicallyDataRepository(covid_driver=CovidDataDriver())
+        data = repository.get_monthly_data(since="2021.1", upto="2021.5")
+        date = dt.datetime.now()
+        
+        monthup = replace_fillz('2021.5')
+        monthsin = replace_fillz('2021.1')
+
+        self.assertIsInstance(data, Monthly)
+        assert data.values[0]['month'] == monthsin
+        assert data.values[-1]['month'] == monthup
+
+        
