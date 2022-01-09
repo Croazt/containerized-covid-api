d317526479a059c9c688c23b24f187c88b63d5e8
Croazt <fachry.croazt@gmail.com>
2022-01-10 03:20:30 +0700

    [fachry] refactor(yearly_tests) : change repository instantiate object from PeriodicallyDataRepository to YearlyDataRepository

--- tests/yearly_data_test.py
+++ tests/yearly_data_test.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 
 from src.driver.covid_data import CovidDataDriver
-from src.repository.periodically_data_repository import PeriodicallyDataRepository, replace_fillz
+from src.repository.yearly_data_repository import YearlyDataRepository, replace_fillz
 from fastapi.testclient import TestClient
 from src.domain.periodically_data import Yearly
 from fastapi.testclient import TestClient
@@ -11,24 +11,24 @@ from fastapi.testclient import TestClient
 from app import app
 class YearlyData(unittest.TestCase):
     def test_periodically_data_repository_given_empty_value_returns_Yearly_object(self):
-        repository = PeriodicallyDataRepository(covid_driver=CovidDataDriver())
+        repository = YearlyDataRepository(covid_driver=CovidDataDriver())
         data = repository.get_yearly_data()
         self.assertIsInstance(data, Yearly)
 
     def test_periodically_data_repository_given_since_value_returns_Yearly_object_between_the_range_of_since_and_date_now(self):
-        repository = PeriodicallyDataRepository(covid_driver=CovidDataDriver())
+        repository = YearlyDataRepository(covid_driver=CovidDataDriver())
         data = repository.get_yearly_data(since=2021)
         self.assertIsInstance(data, Yearly)
         assert data.values[0]['year'] == 2021
 
     def test_periodically_data_repository_given_since_and_upto_value_returns_Yearly_object_between_the_range_of_since_and_upto(self):
-        repository = PeriodicallyDataRepository(covid_driver=CovidDataDriver())
+        repository = YearlyDataRepository(covid_driver=CovidDataDriver())
         data = repository.get_yearly_data(since=2021,upto=2024)
         self.assertIsInstance(data, Yearly)
         assert data.values[0]['year'] == 2021
 
     def test_periodically_data_repository_given_since_value_that_more_than_upto_value_returns_Empty_Yearly_object(self):
-        repository = PeriodicallyDataRepository(covid_driver=CovidDataDriver())
+        repository = YearlyDataRepository(covid_driver=CovidDataDriver())
         data = repository.get_yearly_data(since=2025,upto=2024)
         self.assertIsInstance(data, Yearly)
         assert data.values == []
